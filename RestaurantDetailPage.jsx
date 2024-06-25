21ecb45519654462529e705c9730c9a94b821aca
Lucia Alzugaray <98998748+lualzugaray@users.noreply.github.com>
2024-06-25 17:13:19 -0300

    style: Cambios de estilos
    Se realiza una actualización visual en RestaurantDetailPage.jsx para mejorar la presentación de la información del restaurante.

--- RestaurantDetailPage.jsx
+++ RestaurantDetailPage.jsx
@@ -1,2 +1,3 @@
 avance: Opiniones y Reseñas de Usuarios
 avance: Se desarrolla la feat para que los propietarios de restaurantes respondan a las opiniones dejadas por los usuarios en la página de detalle de restaurante 
+avance: Se realiza una actualización visual para mejorar la presentación de la información del restaurante
