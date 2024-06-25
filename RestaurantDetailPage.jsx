46e455803ac7fdf04360733ca4fa780741e3e3ea
Lucia Alzugaray <98998748+lualzugaray@users.noreply.github.com>
2024-06-25 17:08:55 -0300

    feat: Avance en feat “Reservas y Gestión de Perfil de Restaurantes”: permitir al propietario de un restaurante responder a las opiniones dejadas por los usuarios
    Se desarrolla la feat para que los propietarios de restaurantes respondan a las opiniones dejadas por los usuarios en la página de detalle de restaurante

--- RestaurantDetailPage.jsx
+++ RestaurantDetailPage.jsx
@@ -1,1 +1,2 @@
 avance: Opiniones y Reseñas de Usuarios
+avance: Se desarrolla la feat para que los propietarios de restaurantes respondan a las opiniones dejadas por los usuarios en la página de detalle de restaurante 
